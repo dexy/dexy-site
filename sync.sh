060de36f38f6fddb957904341f53d0a1beee160f
Ana Nelson <ana@ananelson.com>
2010-11-14 15:04:58 +0000

    Reorganize, move contents into root dir rather than web/ dir. Add docs section.

---
+++ sync.sh
@@ -0,0 +1,12 @@
+dexy -s .
+
+# Want to rename index.pdf files to something more useful.
+for f in `find cache/docs/*/index.pdf`; do
+  v=$(ruby -e "'$f'=~/cache\/docs\/(.+)\/index.pdf/; puts \$1")
+  echo "moving $f to $v.pdf"
+  mv $f cache/docs/$v/$v.pdf
+done
+
+# TODO use --delete tag when able to ensure extraneous files not left on server
+rsync -r -v cache/* anaslist@ananelson.com:/home/anaslist/sites/dexy.it/
+
