50ce18dd03863b9e74e35774bd1c5d041182a07a
Ana Nelson <ana@ananelson.com>
2011-06-05 14:53:43 +0100

    Skip clojure example for now. Tweaks to automated builds scripts. Add a run-dexy.sh script, use regexps for excluding directories.

--- install/run-builds.sh
+++ install/run-builds.sh
@@ -5,6 +5,8 @@ function run_script_in_ec2 {
   temp_filename=/tmp/user-data-script.$RANDOM.$$.sh
   echo $temp_filename
 
+  script_dir=`pwd`
+
   script=$1
   script_name_length=${#script}
   script_name=${script:0:($script_name_length-3)}
@@ -13,14 +15,15 @@ function run_script_in_ec2 {
   cat script-header.sh > $temp_filename
   echo "FOLDER_NAME=\"/dexy-builds/\$TIMESTAMP--$script_name--$ami_id\"" >> $temp_filename
   echo "script_name=$script_name" >> $temp_filename
+  echo "cd /mnt" >> $temp_filename
   echo "mkdir $script_name" >> $temp_filename
   echo "cd $script_name" >> $temp_filename
   echo "cat > script.sh <<END" >> $temp_filename
-  cat ~/dev/dexy-site/install/$1 >> $temp_filename
+  cat $script_dir/$1 >> $temp_filename
   echo "END" >> $temp_filename
   echo "bash script.sh &> out.txt" >> $temp_filename
   echo "cd .." >> $temp_filename
-  cat ~/dev/dexy-site/install/close.sh >> $temp_filename
+  cat $script_dir/close.sh >> $temp_filename
 
   ec2-run-instances $ami_id -k $EC2_KEYPAIR \
     -t $3 -f $temp_filename --instance-initiated-shutdown-behavior terminate
@@ -34,7 +37,7 @@ CUSTOM_AMI="ami-92c13cfb"
 cd ~/.ec2
 
 ### @export "run-scripts"
-run_script_in_ec2 min-install-ubuntu.sh $UBUNTU_AMI t1.micro
+#run_script_in_ec2 min-install-ubuntu.sh $UBUNTU_AMI t1.micro
+#run_script_in_ec2 virtualenv-install.sh $CUSTOM_AMI t1.micro
 run_script_in_ec2 source-install.sh $CUSTOM_AMI t1.micro
-run_script_in_ec2 virtualenv-install.sh $CUSTOM_AMI t1.micro
 
