d39b08bc1f82ea8b52995bd1f559f3303657fa4b
Ana Nelson <ana@ananelson.com>
2011-06-05 20:32:35 +0100

    Remove run-dexy as we don't need to exclude dirs. Restore shutdown of VM.

--- install/close.sh
+++ install/close.sh
@@ -6,4 +6,4 @@ tar -czvf $script_name.tgz $script_name
 tar -czvf syslog.tgz -C /var/log syslog
 /home/ubuntu/s3-put -k $AWS_ACCESS_KEY_ID -s /home/ubuntu/secret.txt -T syslog.tgz $FOLDER_NAME/syslog-$TIMESTAMP.tgz
 
-#shutdown -h now
+shutdown -h now
