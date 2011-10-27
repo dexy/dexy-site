241b09b5485e82ef772d7d1f1e82ac06502e9f41
Ana Nelson <ana@ananelson.com>
2011-10-27 21:43:45 +0000

    Separate menu and footer into separate files for sharing between home and other pages.

--- docs/filters/examples/ps/ps-example.py
+++ docs/filters/examples/ps/ps-example.py
@@ -1,17 +1,16 @@
 import turtle
 
 screen = turtle.Screen()
-screen.setworldcoordinates(-11, -11, 11, 11)
 
 turtle.mode('world')
 
 turtle.hideturtle()
-turtle.tracer(1000) # don't update image until the end
+turtle.tracer(1000) # Don't render until the end...
 
 for i in range(12):
-    turtle.circle(100)
+    turtle.circle(50)
     turtle.rt(30)
 
-turtle.update() # now update the image
+turtle.update() # ...now render
 
 print screen.getcanvas().postscript()
--- gallery/index.md
+++ gallery/index.md
@@ -1,10 +1,8 @@
 A showcase of documents created with Dexy.
 
-
 ## Templates
-
 {% for k in sorted(d) -%}
-{% if k.startswith("templates/") -%}
+{% if k.startswith("templates/") and k.endswith("template.html|jinja") -%}
 {{ k }}
 {% endif %}
 {% endfor %}
